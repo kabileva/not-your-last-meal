8accf5ac45ce64caf9efef25036e65bbe74a20bd
Matsbla <mats.gbproject@gmail.com>
2017-07-19 23:00:40 +0200

    name IS alergen friendly variable

--- objects/restaurant.php
+++ objects/restaurant.php
@@ -4,9 +4,9 @@ require_once "node.php";
 
 class Restaurant extends Node {
 
-  private $name, $country, $location, $presentation, $image, $type, $is_menu_friendly;
+  private $name, $country, $location, $presentation, $image, $type, $is_menu_friendly, $is_alergen_friendly;
 
-  function __construct($name, $country, $presentation, $image, $type, $is_menu_friendly, $alergen_friendly, $comments){
+  function __construct($name, $country, $presentation, $image, $type, $is_menu_friendly, $is_alergen_friendly, $comments){
     $this->name = $name;
     $this->country = $country;
     $this->location = $location;
@@ -14,7 +14,7 @@ class Restaurant extends Node {
     $this->image = $image;
     $this->type = $type;
     $this->is_menu_friendly = $is_menu_friendly; // Does the restaurant have informative menues?
-    $this->alergen_friendly = $alergen_friendly; // List of alergens that the restaurant have optional food for
+    $this->is_alergen_friendly = $is_alergen_friendly; // List of alergens that the restaurant have optional food for
   }
 
   function createInDatabase(){
