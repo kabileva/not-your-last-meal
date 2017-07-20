53a0eafe583fef0f4a0b457bb47514867b5a2e65
ihl21 <ihl99637953@gmail.com>
2017-07-20 21:57:15 +0900

    updated register form- allergen

---
+++ forms/restaurant.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        
+        <h1><?php
+            echo $name;
+            ?></h1><br>
+        
+        <p>Location<br><?php echo $location;?></p>
+          
+        <img src="" alt="not found" width="" height="">
+                
+    </body>
+</html>
