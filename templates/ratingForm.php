cddf4200c032c0d947e2251636a3c85742223e2d
kmh9409 <kmh9409@naver.com>
2017-07-25 00:04:19 +0900

    Update ratingForm.php

--- templates/ratingForm.php
+++ templates/ratingForm.php
@@ -1,35 +1,44 @@
-<script>
-    function validateForm(){
+<html>
+<head>
+</head>
 
-        var comment=document.forms["ratForm"]["star"].value;
-        var count= 0;
+<body>
 
-        if(comment==""){
-            document.getElementById("commSp").innerHTML="Select Star!";
-            count++;
-        }
+  <script>
+      function validateForm(){
+          var star=document.forms["ratingForm"]["star"].value;
+          var count= 0;
+          if(star==""){
+
+             document.getElementById("starSp").innerHTML="Required area";
+
+              count++;
+          }
+          if(count==0)
+              return true;
+          else
+              return false;
+      }
+  </script>
+
+<h1>Star rating:</h1><br>
 
 
-        if(count==0)
-            return true;
-        else
-            return false;
-    }
-
-</script>
-
-
-<form name="ratForm" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" onsubmit="return validateForm()">
-    <fieldset>
-        <legend>Rating</legend>
-        <input type="radio" name="star" value="0">☆☆☆☆☆
-        <input type="radio" name="star" value="1">★☆☆☆☆
-        <input type="radio" name="star" value="2">★★☆☆☆
-        <input type="radio" name="star" value="3">★★★☆☆
-        <input type="radio" name="star" value="4">★★★★☆
-        <input type="radio" name="star" value="5">★★★★★
-    </fieldset>
-    <fieldset>
-        <input type="submit" name="submit" value="Submit"> <span id="commSp"></span>
-    </fieldset>
+<form name="ratingForm" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" onsubmit="return validateForm()">
+<fieldset>
+<input type="radio" name="star" value="0">☆☆☆☆☆
+<input type="radio" name="star" value="1">★☆☆☆☆
+<input type="radio" name="star" value="2">★★☆☆☆
+<input type="radio" name="star" value="3">★★★☆☆
+<input type="radio" name="star" value="4">★★★★☆
+<input type="radio" name="star" value="5">★★★★★
+<br><span id="starSp"></span>
+<br><br>
+</fieldset>
+<fieldset>
+<input type="submit" name="submit" value="Submit">
+</fieldset>
 </form>
+
+</body>
+</html>
