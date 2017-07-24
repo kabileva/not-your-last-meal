4cdab8ddfd2d1b46c83276466e0a9e49fbb0442d
ihl21 <ihl99637953@gmail.com>
2017-07-24 22:19:12 +0900

    questionForm

---
+++ questionForm.php
@@ -0,0 +1,48 @@
+<script>
+    function quesvalidateForm(){
+
+        var title=document.forms["quesForm"]["title"].value;
+        var question=document.forms["quesForm"]["question"].value;
+        var count= 0;
+
+        if((title=="")||(question=="")){
+            if(title==""){
+                document.getElementById("titleSp").innerHTML="Required area";
+            }
+            if(question==""){
+                document.getElementById("quesSp").innerHTML="Required area";
+            }
+            count++;
+        }
+
+
+        if(count==0)
+            return true;
+        else
+            return false;
+    }
+
+</script>
+
+<form name="quesForm" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" onsubmit="return quesvalidateForm()">
+    
+    
+    <fieldset>
+        <legend>Title</legend>
+        <input type="text" name="title"> <span id="titleSp"></span>
+    </fieldset>
+    
+    
+    <fieldset>
+        <legend>Post a question</legend>
+        <textarea name="question" rows="10" cols="60"></textarea>
+        <span id="quesSp"></span>
+    </fieldset>
+    
+    
+    <fieldset>
+        <input type="submit" value="Confirm"> <input type="reset" value="Clear">
+    </fieldset>
+    
+    
+</form>
