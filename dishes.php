977809fc66a5b6e9cb61a3a04aa2736e8789ab08
Matsbla <mats.gbproject@gmail.com>
2017-07-24 16:46:16 +0200

    Add breadcrumb

--- dishes.php
+++ dishes.php
@@ -3,14 +3,21 @@
 
 include_once 'templates/layout.php';
 
-include_once 'templates/go-back.php';	
+include_once 'TEST-DATA.php';
 
 ?>
+
+<ul class="breadcrumb">
+  <li><a href="main.php"><?php print($userCountry) ?></a></li>
+  <li><a href="dishes.php">Dishes</a></li>
+</ul>
+
+<a href="dishForm.php" class="callToAction">Add dish<a>
 
 <h1>Dishes</h1>
 <title>Dishes</title>
 
-<a href="dishForm.php" class="callToAction">Add dish<a>
+
 
 <div id="main">
    <a href="info.html">
--- forum.php
+++ forum.php
@@ -1,14 +1,18 @@
 <!DOCTYPE HTML>
 <?php
 
-include_once 'templates/layout.php';
+include_once 'templates/layout.php';	
 
-include_once 'templates/go-back.php';		
+include_once 'TEST-DATA.php';
 
 ?>
 
-<title>Question and answers</title>
-<h1>Question and answers</h1>
+<ul class="breadcrumb">
+  <li><a href="main.php"><?php print($userCountry) ?></a></li>
+  <li><a href="forum.php">Questions and answers</a></li>
+</ul>
 
 <a href="questionForm.php" class="callToAction">Ask a question<a>
 
+<title>Question and answers</title>
+<h1>Question and answers</h1>
