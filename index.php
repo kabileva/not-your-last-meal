4f9201f6d888bdc0c6072658c06aaa55c0951aa9
Matsbla <mats.gbproject@gmail.com>
2017-07-24 15:32:57 +0200

    Fix template links

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ include_once 'templates/head.php';
    <div class="login-wrapper">
       <div class="form">
          <h3 class="title">Log in</h3>
-         <?php include_once 'forms/login.php'?>
+         <?php include_once 'templates/loginForm.php'?>
       </div>
       <div class="form">
          <h3 class="title">Register</h3>
-         <?php include_once 'forms/register.php'?>
+         <?php include_once 'templates/registerForm.php'?>
       </div>
    </div>
 </body>
