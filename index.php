262c8126237df66cfc48b4b1b7d8930f8334f38d
Matsbla <mats.gbproject@gmail.com>
2017-07-24 15:37:42 +0200

    Fix login css

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE HTML>
-<?php
 
-include_once 'templates/head.php';
-   
-?>
+<head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width">
+   <link rel="stylesheet" href="css/login.css">
+   <link rel="stylesheet" href="css/styles.css">
+   <link href="https://fonts.googleapis.com/css?family=Quicksand:300,700" rel="stylesheet">
+</head>
 
 <title>Not your last meal</title>
 <body>
