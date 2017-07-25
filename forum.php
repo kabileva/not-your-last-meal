9110d15f9d23b378df88c88ecb6ef758d0aa1227
Matsbla <mats.gbproject@gmail.com>
2017-07-25 03:45:00 +0200

    Add test data for questions

--- forum.php
+++ forum.php
@@ -3,7 +3,12 @@
 
 include_once 'templates/layout.php';	
 
+include_once 'objects/question.php';
+
+// Some test data to be removed when database is implemented
 include_once 'TEST-DATA.php';
+$testQuestion1 = new Question('Im alergic to milk, what do I need to think about?', 'So let me explain about my problem, bla bla bla bla bla....', 'user1', '1th January 2017', array('Gluten' , 'milk'), '');
+$questionList = array($testQuestion1, $testQuestion1, $testQuestion1, $testQuestion1, $testQuestion1, $testQuestion1, $testQuestion1);
 
 ?>
 
@@ -14,5 +19,25 @@ include_once 'TEST-DATA.php';
 
 <a href="questionForm.php" class="callToAction">Ask a question<a>
 
-<title>Question and answers</title>
-<h1>Question and answers</h1>
+<?php
+  if(isset($_GET['selectedQuestion'])){
+    $content = $testQuestion1->getContent();
+    echo $content;
+    echo '<h2>More questions</h2>';
+  }
+  else {
+    echo'
+      <h1>Questions and answers</h1>
+      <title>Questions and answers</title>
+      <div class="undertitle">Ask and give information to fellow alergic people</div>
+    ';
+
+  }
+?>
+
+<!-- Lists of users -->
+<?php
+foreach($questionList as $question){
+  $teaser = $question->getTeaser();
+  echo $teaser;
+}
