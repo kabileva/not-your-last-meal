34997c61cc13ea45a25cebd4c68ee86fa4403dab
goodCycle <kitiyj526@gmail.com>
2017-07-25 12:45:28 +0200

    modify to use the same db with server db

--- templates/loginHandle.php
+++ templates/loginHandle.php
@@ -8,47 +8,6 @@
 	$errors = array();
 	$_SESSION['success'] = "";
 
-	// connect to database
-<<<<<<< HEAD
-	$db = mysqli_select_db($link, 'notlastmeal');
-=======
-	$db = mysqli_connect('localhost', 'root', 'root', 'id2305220_data');
-
->>>>>>> bc2f698ac38e412e9e597565cd84ef30ebb913c6
-
-	// REGISTER USER
-	/*if (isset($_POST['reg_user'])) {
-		// receive all input values from the form
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
-		// form validation: ensure that the form is correctly filled
-		if (empty($username)) { array_push($errors, "Username is required"); }
-		if (empty($email)) { array_push($errors, "Email is required"); }
-		if (empty($password_1)) { array_push($errors, "Password is required"); }
-
-		if ($password_1 != $password_2) {
-			array_push($errors, "The two passwords do not match");
-		}
-
-		// register user if there are no errors in the form
-		if (count($errors) == 0) {
-			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO users (username, email, password)
-					  VALUES('$username', '$email', '$password')";
-			mysqli_query($db, $query);
-
-			$_SESSION['username'] = $username;
-			$_SESSION['success'] = "You are now logged in";
-			header('location: index.php');
-		}
-
-	}*/
-
-	// ...
-
 	// LOGIN USER
 	if (isset($_POST['login_user'])) {
 		$username = $_POST['username'];
