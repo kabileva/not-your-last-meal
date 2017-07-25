50663789d70f2f1f18ba46fd5fb9503083dfbd20
Matsbla <mats.gbproject@gmail.com>
2017-07-25 03:13:28 +0200

    Add test data for restaurants

--- restaurants.php
+++ restaurants.php
@@ -3,8 +3,12 @@
 
 include_once 'templates/layout.php';	
 
-include_once 'TEST-DATA.php';
+include_once 'objects/restaurant.php';
 
+// Some test data to be removed when database is implemented
+include_once 'TEST-DATA.php';
+$testRestaurant1 = new Restaurant('Restaurantname', 'Kazakstan', array('log','lat'), 'Enjou your meal in this cool place', 'test-data/images/kazakstan-eating.jpg', 'Dinner', TRUE, array('gluten', 'nuts'), '');
+$restaurantList = array($testRestaurant1, $testRestaurant1, $testRestaurant1, $testRestaurant1, $testRestaurant1, $testRestaurant1, $testRestaurant1);
 ?>
 
 <ul class="breadcrumb">
@@ -14,5 +18,25 @@ include_once 'TEST-DATA.php';
 
 <a href="restaurantForm.php" class="callToAction">Add restaurant<a>
 
-<title>Restaurants</title>
-<h1>Restaurants</h1>
+<?php
+  if(isset($_GET['selectedRestaurant'])){
+    $content = $testRestaurant1->getContent();
+    echo $content;
+    echo '<h2>More restaurants</h2>';
+  }
+  else {
+    echo'
+      <h1>Restaurants</h1>
+      <title>Restaurants</title>
+      <div class="undertitle">Find eating places where you can eat safely at your destination</div>
+    ';
+
+  }
+?>
+
+<!-- Lists of users -->
+<?php
+foreach($restaurantList as $restaurant){
+  $teaser = $restaurant->getTeaser();
+  echo $teaser;
+}
