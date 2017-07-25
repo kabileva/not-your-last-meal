75a6d9186ab8d7191dc1ec139b039ffcdb0685ab
goodCycle <kitiyj526@gmail.com>
2017-07-25 17:37:50 +0200

    solve problems about session on login

--- templates/loginHandle.php
+++ templates/loginHandle.php
@@ -1,20 +1,11 @@
 <?php
 
-
 	// LOGIN USER
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    echo "we are in post ";
-    //include_once("../db/db_init.php");
+  
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		/*$username2 = stripcslashes($username);
-	  $password2 = stripcslashes($password);
-	  $username = mysqli_real_escape_string($username);
-	  $password = mysqli_real_escape_string($password);*/
 
-    echo $username;
-    echo "<br>";
-    echo $password;
 
 		if (empty($username)) {
 			array_push($errors, "Username is required");
@@ -24,29 +15,28 @@
 		}
 
 		if (count($errors) == 0) {
-      echo "no errors ";
+
       $conn = new mysqli('localhost','root', 'root', 'id2305220_data');
+
       // Check connection
       if (!$conn) {
           die("Connection failed: "  . mysqli_connect_error());
       }
-      //$link = mysqli_connect('localhost', 'root', 'root');
-      //$db = mysqli_select_db($link, 'id2305220_data');
-			$sql = "SELECT Password, UserName, Email, Image, Presentation, countryName
+      //Password, UserName, Email, Image, Presentation, CountryName
+      $sql = "SELECT *
                 FROM users LEFT JOIN countries
                 ON users.CountryID = countries.CountryID
                 WHERE username='$username' AND password='$password'";
 
       $result = mysqli_query($conn, $sql);
-
-      echo "hey you are in no errors";
 		  $row = mysqli_fetch_assoc($result);
 
 			if (mysqli_num_rows($result) > 0) {
         session_start();
-        //header('location: ../dishForm.php');
-        $_SESSION['userName'] = $row['userName'];
-        $_SESSION['userCountry'] = $row['countryName'];
+
+        $_SESSION['userName'] = $row['UserName'];
+        $_SESSION['userCountry'] = $row['CountryName'];
+        $_SESSION['selectedCountry'] = $row['CountryID'];
         $_SESSION['userImage'] = $row['Image'];
 
 				header('location: ../main.php');
