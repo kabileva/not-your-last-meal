bc36242f497c6f93372e7fa8d848bf1a36354bb9
Matsbla <mats.gbproject@gmail.com>
2017-07-25 20:08:09 +0200

    Fix small mistake

--- main.php
+++ main.php
@@ -25,7 +25,7 @@ $testUser1 = new User("User1", "test@test.com", "myPassword", TRUE, "Argentina",
 $userList = array($testUser1, $testUser1, $testUser1, $testUser1, $testUser1, $testUser1, $testUser1, $testUser1);
 ?>
 
-<title>Not your last meal - <?php print($_SESSION['selectedCountry'] ?></title>
+<title>Not your last meal - <?php echo $_SESSION['selectedCountry']; ?></title>
 <h1><?php print($userCountry) ?></h1>
 
 <div id="main">
